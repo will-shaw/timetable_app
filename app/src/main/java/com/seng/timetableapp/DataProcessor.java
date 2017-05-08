5c4c71656b308f7fc4d40a902640715de3375426
shaun <shaundavidkerr@gmail.com>
2017-05-08 15:21:14 +1200

    Refactored helper functions

--- app/src/main/java/com/seng/timetableapp/DataProcessor.java
+++ app/src/main/java/com/seng/timetableapp/DataProcessor.java
@@ -1,67 +1,13 @@
 package com.seng.timetableapp;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Created by shaun on 5/8/2017.
  */
 
 public class DataProcessor {
-
-    /**
-     * Helper function to trim unnecessary characters from the input string.
-     * @param c The character to trim to
-     * @param s The string to trim
-     * @return The substring starting from the first occurence of c, to the end of the string, or s if the character isn't found.
-     */
-    private String nextChar(char c, String s) {
-        boolean f = false;
-        int i;
-        for(i = 0; (i < s.length() && !f); i++) {
-            if(s.charAt(i) == (c)) {
-                f = true;
-            }
-        }
-        if(f) {
-            return s.substring(i);
-        }
-        return s;
-    }
-
-    /**
-     * Helper function to return a block defined by cS and cE (Such as a pair of brackets)
-     * @param cS Starting character of block
-     * @param cE Ending character of block
-     * @param s String to work on
-     * @return The substring starting with cS and ending with cE, or null if no block exists.
-     */
-    private String getBlock(char cS, char cE, String s) {
-        String t = nextChar(cS, s);
-        //Handle if input string was already entire block, or if it did not contain cS.
-        if(t.charAt(0) == (cS) && t.charAt(t.length()-1) == (cE)) {
-            return t;
-        } else if(t.equals(s)) {
-            return null;
-        }
-        //Actual function
-        int nests = 0;
-        int i;
-        for(i = 0; i < t.length(); i++) {
-            if(t.charAt(i) == cS) {
-                ++nests;
-            } else if(t.charAt(i) == cE) {
-                --nests;
-            }
-            //If nests is zero, we've finished the block and can return it.
-            if(nests == 0) {
-                return t.substring(0,i+1);
-            }
-            //If nests is negative, brackets arent set up properly.
-            if(nests < 0) {
-                return null;
-            }
-        }
-        //If it reaches the end of the loop something is mismatched.
-        return null;
-    }
 
     public class TTEvent {
         private String id;
@@ -181,6 +127,69 @@ public class DataProcessor {
         public void setStream(String stream) {
             this.stream = stream;
         }
+    }
+
+    /**
+     * Helper function to trim unnecessary characters from the input string.
+     * @param c The character to trim to
+     * @param s The string to trim
+     * @return The substring starting from the first occurence of c, to the end of the string, or s if the character isn't found.
+     */
+    private String nextChar(char c, String s) {
+        boolean f = false;
+        int i;
+        for(i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == (c)) {
+                f = true;
+                break;
+            }
+        }
+        if(f) {
+            return s.substring(i);
+        }
+        return s;
+    }
+
+    /**
+     * Helper function to return a block defined by cS and cE (Such as a pair of brackets)
+     * @param cS Starting character of block
+     * @param cE Ending character of block
+     * @param s String to work on
+     * @return An array of strings, each being a block as defined by cS and cE, null if mismatched.
+     */
+    private Collection<String> getBlock(char cS, char cE, String s) {
+        Collection<String> blocks = new ArrayList<>();
+        String t = nextChar(cS, s);
+        int nests = 0;
+        int start = 0;
+        boolean lookForStart = false;
+        int i;
+        for(i = 0; i < t.length(); i++) {
+            //If starting a new block, skip over in-between characters
+            if(lookForStart && t.charAt(i) != cS) {
+                continue;
+            } else {
+                lookForStart = false;
+                start = i;
+            }
+            if(t.charAt(i) == cS) {
+                ++nests;
+            } else if(t.charAt(i) == cE) {
+                --nests;
+            }
+            //If nests is zero, we've finished the block and can return it.
+            if(nests == 0) {
+                blocks.add(t.substring(start,i+1));
+                lookForStart = true;
+            }
+            //If nests is negative, brackets aren't set up properly.
+            if(nests < 0) {
+                return null;
+            }
+        }
+        //Anything not added will be extra characters outside of a block, so ignore them
+        //and return the blocks we have found.
+        return blocks;
     }
 }
 
