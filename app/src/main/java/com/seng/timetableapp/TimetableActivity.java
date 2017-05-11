c0a9ed8a888de55668aa040f264d4fb7e4ed1549
will-shaw <will_shaw@live.com>
2017-05-11 21:37:41 +1200

    Updated timetable activity to open a view event activity on item press.

--- app/src/main/java/com/seng/timetableapp/TimetableActivity.java
+++ app/src/main/java/com/seng/timetableapp/TimetableActivity.java
@@ -1,13 +1,17 @@
 package com.seng.timetableapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -118,8 +122,28 @@ public class TimetableActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(ArrayList<TimetableItem> timetable) {
 
-            ListView listToday = (ListView) findViewById(R.id.list_tt_today);
-            ListView listTomorrow = (ListView) findViewById(R.id.list_tt_tomorrow);
+            final ListView listToday = (ListView) findViewById(R.id.list_tt_today);
+            final ListView listTomorrow = (ListView) findViewById(R.id.list_tt_tomorrow);
+
+            listToday.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String label = (String) listToday.getItemAtPosition(position);
+                    Intent intent = new Intent(TimetableActivity.this, ViewEventActivity.class);
+                    intent.putExtra("paper", label);
+                    startActivity(intent);
+                }
+            });
+
+            listTomorrow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String label = (String) listTomorrow.getItemAtPosition(position);
+                    Intent intent = new Intent(TimetableActivity.this, ViewEventActivity.class);
+                    intent.putExtra("paper", label);
+                    startActivity(intent);
+                }
+            });
 
             //Creates string arrays for today and tomorrow
             for (TimetableItem ttItem: timetable) {
