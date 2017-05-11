2613c0405ea45f266a3d02215296f75cc35875bc
shaun <shaundavidkerr@gmail.com>
2017-05-11 12:26:55 +1200

    Created TTEvent class with default getters and setters.

---
+++ app/src/main/java/com/seng/timetableapp/TTEvent.java
@@ -0,0 +1,152 @@
+import java.util.Date;
+
+/**
+ * Universal Event Class
+ *    So that all parts of code have a consistent standard for data io.
+ */
+public class TTEvent {
+   private String id;
+   private Date date;
+   private int day;
+   private int start;
+   private int end;
+   private String bcol;
+   private String fcol;
+   private String lectureName;
+   private String gmapsUrl;
+   private double lat;
+   private double lon;
+   private String roomCode;
+   private String paperName;
+   private String roomName;
+   private String buildingName;
+   private String stream;
+   
+   public Date getDate() {
+      return date;
+   }
+
+   public void setDate(Date date) {
+      this.date = date;
+   }
+
+   public double getLat() {
+      return lat;
+   }
+
+   public void setLat(double lat) {
+      this.lat = lat;
+   }
+
+   public double getLon() {
+      return lon;
+   }
+
+   public void setLon(double lon) {
+      this.lon = lon;
+   }
+
+   public String getId() {
+      return id;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   public int getDay() {
+      return day;
+   }
+
+   public void setDay(int day) {
+      this.day = day;
+   }
+
+   public int getStart() {
+      return start;
+   }
+
+   public void setStart(int start) {
+      this.start = start;
+   }
+
+   public int getEnd() {
+      return end;
+   }
+
+   public void setEnd(int end) {
+      this.end = end;
+   }
+
+   public String getBcol() {
+      return bcol;
+   }
+
+   public void setBcol(String bcol) {
+      this.bcol = bcol;
+   }
+
+   public String getFcol() {
+      return fcol;
+   }
+
+   public void setFcol(String fcol) {
+      this.fcol = fcol;
+   }
+
+   public String getLectureName() {
+      return lectureName;
+   }
+
+   public void setLectureName(String lectureName) {
+      this.lectureName = lectureName;
+   }
+
+   public String getGmapsUrl() {
+      return gmapsUrl;
+   }
+
+   public void setGmapsUrl(String gmapsUrl) {
+      this.gmapsUrl = gmapsUrl;
+   }
+
+   public String getRoomCode() {
+      return roomCode;
+   }
+
+   public void setRoomCode(String roomCode) {
+      this.roomCode = roomCode;
+   }
+
+   public String getPaperName() {
+      return paperName;
+   }
+
+   public void setPaperName(String paperName) {
+      this.paperName = paperName;
+   }
+
+   public String getRoomName() {
+      return roomName;
+   }
+
+   public void setRoomName(String roomName) {
+      this.roomName = roomName;
+   }
+
+   public String getBuildingName() {
+      return buildingName;
+   }
+
+   public void setBuildingName(String buildingName) {
+      this.buildingName = buildingName;
+   }
+
+   public String getStream() {
+      return stream;
+   }
+
+   public void setStream(String stream) {
+      this.stream = stream;
+   }
+}
